a3fd144cc844f837be2e2161067f75ddb906fc12
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-08-15 19:37:02 +0100

    a script that deleted a database updated

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,5 +1,12 @@
--- a script that deletes a data base
+-- -- a script that deletes a data base
+
+-- DROP DATABASE IF EXISTS hbtn_0c_0;
+
+-- CREATE DATABASE hbtn_0c_0;
+
+
+-- a script that creates the database hbtn_0c_0 in your MySQL server.
 
 DROP DATABASE IF EXISTS hbtn_0c_0;
 
-CREATE DATABASE hbtn_0c_0;
+CREATE SCHEMA hbtn_0c_0;
