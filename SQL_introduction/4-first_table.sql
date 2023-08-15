2b0a2983d192f6023d3866a8872e6881050c7b05
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-08-15 12:06:22 +0100

    Script that creates a table called first_table in the current database in your MySQL server.

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,11 @@
+--Write a script that creates a table called first_table in the current database in your MySQL server.
+
+USE hbtn_0c_0;
+
+DROP TABLE IF EXISTS first_table;
+
+CREATE TABLE first_table (
+	id int,
+	name varchar(256)
+);
+
