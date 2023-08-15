4490e87b0700862bd849287406e39a4abe0b9c7b
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-08-15 19:27:39 +0100

    a script that lists all tables from a database updated

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,12 +1,5 @@
---a script that lists all the tables of a database in your MySQL server
-
-
--- USE httn_test_db_;
-
--- SHOW TABLES;
-
 --  a script that lists all the tables of a database in your MySQL server.
 
-USE hbtn_test_db_3;
+USE hbtn_test_db_0;
 
 SHOW TABLES;
