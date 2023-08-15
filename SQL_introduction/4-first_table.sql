34bdc66e84bcec06e988e357041e5cea3db3a6c4
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-08-15 19:20:09 +0100

    a script that creates data in the first table updated

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,11 +1,11 @@
 --Write a script that creates a table called first_table in the current database in your MySQL server.
 
-USE hbtn_test_db_5;
+USE hbtn_test_db_4;
 
 DROP  TABLE IF EXISTS first_table;
 
-CREATE TABLE IF NOT EXISTS first_table (
-	id int,
+CREATE TABLE first_table (
+	id int, 
 	name varchar(256)
 );
 
