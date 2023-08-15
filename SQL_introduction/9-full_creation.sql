f3fb7f48ac429ac351f993683f1d65d9c0011fe8
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-08-15 19:07:29 +0100

    a script that creates a second table with some details updated

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,24 +1,42 @@
--- a script that creates a table second_table in the database 
+-- -- a script that creates a table second_table in the database 
 
--- The database
+-- -- The database
+-- USE hbtn_test_db_9;
+
+-- -- creating a table
+-- CREATE TABLE IF NOT EXISTS second_table (
+-- 	id INT,
+-- 	name VARCHAR(256),
+-- 	score INT
+-- );
+
+-- -- Insecting New Records
+-- INSECT INTO second_table
+-- VALUES(1, 'John', 10);
+
+-- INSECT INTO second_table
+-- VALUES(2, 'Alex', 3);
+
+-- INSECT INTO second_table
+-- VALUES(3, 'Bob', 14);
+
+-- INSECT INTO second_table
+-- VALUES(4, 'George', 8);
+
+-- Write a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
+
 USE hbtn_test_db_9;
 
--- creating a table
-CREATE TABLE IF NOT EXISTS second_table (
-	id INT,
-	name VARCHAR(256),
-	score INT
-);
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 
--- Insecting New Records
-INSECT INTO second_table
+INSERT INTO second_table
 VALUES(1, 'John', 10);
 
-INSECT INTO second_table
+INSERT INTO second_table
 VALUES(2, 'Alex', 3);
 
-INSECT INTO second_table
+INSERT INTO second_table
 VALUES(3, 'Bob', 14);
 
-INSECT INTO second_table
+INSERT INTO second_table
 VALUES(4, 'George', 8);
