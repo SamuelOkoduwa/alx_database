78d8650ce9f8967d46320409d922537b17b554ae
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-08-15 14:09:06 +0100

    a script that prints the full description of the table

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,7 @@
 --write a script that prints the full description of the table
 
 USE hbtn_0c_0;
-SHOW CREATE TABLE first_table;
+-- SHOW CREATE TABLE first_table;
+SHOW COLUMNS FROM first_table FROM hbtn_0c_0;
 
 
