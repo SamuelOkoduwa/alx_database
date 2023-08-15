04b450d26e60f02a12b2e4e82b9a30912ce81c9a
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-08-15 14:20:19 +0100

    Write a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,6 @@
 --write a script that prints the full description of the table
 
-USE hbtn_0c_0;
+USE hbtn_test_db_6;
 -- SHOW CREATE TABLE first_table;
 SHOW COLUMNS FROM first_table FROM hbtn_0c_0;
 
