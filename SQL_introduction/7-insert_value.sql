a2db587f0979eb955959706690e4043f74b8b92e
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-08-15 18:02:08 +0100

    Write a script that inserts a new row in a table update

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,7 +1,5 @@
---Write a script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server
+-- a script that inserts a new row in the table first_table
 
 USE hbtn_test_db_7;
-
 INSERT INTO first_table 
-	VALUES(89, 'Holberton School'
-);
+VALUES (89, 'Holberton School');
