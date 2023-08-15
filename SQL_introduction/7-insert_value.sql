16eecb071b4b30f3fd190cddcdec20e943ddcd72
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-08-15 17:58:51 +0100

    Write a script that inserts a new row in a table

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,5 +1,6 @@
---
-USE hbtn_test_db_6;
+--Write a script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server
+
+USE hbtn_test_db_7;
 
 INSERT INTO first_table VALUES(
 	89, "Holberton School"
