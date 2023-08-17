e9e2ed2beb2e7160d77240dc5e9feef295342d5e
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-08-17 17:59:25 +0100

    a script that creates the database hbtn_0d_2 and the user updated

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- write a script that creates the database hbtn_0d_2 and the ueser
 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE DATABASE IF EXISTS hbtn_0d_2;
+CREATE USER IF EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost'; 
